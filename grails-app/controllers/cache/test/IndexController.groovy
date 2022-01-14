1dbb4b52a54ef483b36e57468d5448bd1b275789
demon <chartgizmo@gmail.com>
2022-01-14 21:29:07 +0100

    example

--- grails-app/controllers/cache/test/IndexController.groovy
+++ grails-app/controllers/cache/test/IndexController.groovy
@@ -3,9 +3,10 @@ package cache.test
 class IndexController {
 
     def index() {
+        log.error "Request start"
         Test.get(1l)
         TestAA.get(1l)
-
+        log.error " == done == "
         render "ok"
     }
 
